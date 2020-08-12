c82999a728b0b0e84aaf95a4a377420acb15713b
kimayaBasu06 <62259697+kimayaBasu06@users.noreply.github.com>
2020-08-11 18:37:15 -0700

    Update index.js

--- PrivatePhotoG/pages/index.js
+++ PrivatePhotoG/pages/index.js
@@ -35,37 +35,35 @@ export default function Blog() {
 
       <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
+          <img 
+            className={styles.gracie} 
+            src="/famphoto.png" 
+            alt="my image" 
           />
-          <Carousel.Caption>
+          <Carousel.Caption className={styles.merry}>
             <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Third slide"
+          <img 
+            className={styles.gracie} 
+            src="/family_history.jpg" 
+            alt="my image" 
           />
-          <Carousel.Caption>
+          <Carousel.Caption className={styles.go}>
             <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
+          <img 
+            className={styles.gracie} 
+            src="/cheeny.JPG" 
+            alt="my image" 
           />
 
-          <Carousel.Caption>
+          <Carousel.Caption className={styles.go}>
             <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -99,3 +97,8 @@ export default function Blog() {
 
 
 }
+
+
+
+
+
